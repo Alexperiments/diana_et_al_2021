ceaa036203afa89fcb9c50175cea6667a58461f3
AleD1996 <dianaalessandro96@gmail.com>
2021-10-10 23:52:43 +0200

    Update parameter_estimation file

--- parameter_estimation.py
+++ parameter_estimation.py
@@ -8,11 +8,76 @@ from scipy.optimize import curve_fit
 import json
 
 
-def gaussian(x, A, mean1, sigma1, B, mean2, sigma2):
+def gaussian1(x, A, mean1, sigma1):
+    return A*np.exp(-np.power((x-mean1)/sigma1,2.) / 2.)
+
+def gaussian2(x, A, mean1, sigma1, B, mean2, sigma2):
     return (A*np.exp(-np.power((x-mean1)/sigma1,2.) / 2.) +
             B*np.exp(-np.power((x-mean2)/sigma2,2.) / 2.))
-            #C*np.exp(-np.power((x-mean3)/sigma3,2.) / 2.)
 
+def gaussian3(x, A, mean1, sigma1, B, mean2, sigma2, C, mean3, sigma3):
+    return (A*np.exp(-np.power((x-mean1)/sigma1,2.) / 2.) +
+            B*np.exp(-np.power((x-mean2)/sigma2,2.) / 2.) +
+            C*np.exp(-np.power((x-mean3)/sigma3,2.) / 2.))
+
+init_guess1 = [ 1, 1549, 30 ]
+init_guess2 = [ 1, 1549, 30, 1, 1549, 30 ]
+init_guess3 = [ 1, 1549, 30, 1, 1549, 30, 1, 1549, 30 ]
+
+
+class Choose_a_fit:
+    def __init__(self, fig):
+        self.continuum = int_dict['continuum']
+        self.masks = []
+        self.cid2 = fig.canvas.mpl_connect('button_press_event', self.on_click)
+        self.cid2 = fig.canvas.mpl_connect('close_event', self.on_close)
+        self.fig = fig
+        self.ax = fig.gca()
+        self.fit_lines = self.ax.lines[5:]
+
+    def on_key(self, event):
+        if event.key == '1':
+            pass
+        elif event.key == '2':
+            pass
+        elif event.key == '3':
+            pass
+        elif event.key == 'f4':
+            if (len(self.continuum) == 4) and (len(self.masks)%2 == 0):
+                int_dict['continuum'] = str(self.continuum)
+                int_dict['masks'] = str(self.masks)
+                name = obj_name + '.png'
+                plt.savefig(config.FITTED_PLOTS+name, dpi=300)
+                plt.close(self.fig)
+            else:
+                print(
+                f"Punti continuo: {len(self.continuum)}/4\tPunti maschera: {len(self.masks)}"
+                )
+
+    def on_click(self, event):
+        if self.continuum_mode:
+            if len(self.continuum) < 4:
+                self.continuum.append(event.xdata)
+            else: print("Ho già 4 punti per il continuo, rimuoverne qualcuno!")
+        elif self.masks_mode: self.masks.append(event.xdata)
+        self.update()
+
+    def on_close(self, event):
+        del self.continuum
+        del self.masks
+
+    def update(self):
+        for line in self.continuum:
+            self.ax.axvline(line, color='green', ls='--')
+        if (len(self.masks)%2 == 0) & (len(self.masks) != 0):
+            self.spans.append(self.ax.axvspan(self.masks[-2], self.masks[-1], alpha=0.5, color='gray'))
+        if len(self.continuum) == 4:
+            m, q = continuum_fit(obj_spectra, self.continuum)
+            int_dict['m'] = m
+            int_dict['q'] = q
+            x_bin = np.arange(self.continuum[0], self.continuum[3], 1)
+            self.ax.plot(x_bin, q + m*x_bin, color='red')
+        self.fig.canvas.draw()
 
 
 for i, obj_name in enumerate(sorted(os.listdir(config.HDF_FOLDER))):
@@ -46,16 +111,10 @@ for i, obj_name in enumerate(sorted(os.listdir(config.HDF_FOLDER))):
     ivar = ivar[mask_condition]
 
     flux = flux - (m*lam + q)
-    init_guess = [
-        1, 1549, 30,
-        1, 1549, 30,
-        #0, 1549, 1
-    ]
-    bounds = [
-        [0, 1510, 0, 0, 1510, 0],
-        [np.inf, 1580, np.inf, np.inf, 1580, np.inf]
-    ]
-    par, pcov = curve_fit(gaussian, lam, flux, p0=init_guess, sigma=np.sqrt(1/ivar), bounds=bounds)
+
+    par1, pcov1 = curve_fit(gaussian1, lam, flux, p0=init_guess1, sigma=np.sqrt(1/ivar))
+    par2, pcov2 = curve_fit(gaussian2, lam, flux, p0=init_guess2, sigma=np.sqrt(1/ivar))
+    par3, pcov3 = curve_fit(gaussian3, lam, flux, p0=init_guess3, sigma=np.sqrt(1/ivar))
 
     for i in range(len(masks)//2):
         plt.axvspan(masks[i*2], masks[i*2+1], alpha=0.5, color='gray')
